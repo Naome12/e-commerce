ffbe391493d59296d13d61ad9ae6859f2f12e85e
Naome <tuyishimenaome27@gmail.com>
2023-10-19 15:15:19 +0300

    adding fonts

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
+import * as SpalshScreen from "expo-splash-screen";
+import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsload] = useFonts({
+    
+  })
   return (
     <View style={styles.container}>
       <Text>to start work on your app</Text>
