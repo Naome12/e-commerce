0e0117f6b20140650de8099190a34e750bdd247e
Naome <tuyishimenaome27@gmail.com>
2023-10-26 19:19:41 +0200

    end

---
+++ components/home/Welcome.jsx
@@ -0,0 +1,24 @@
+import { View, Text,TouchableOpacity } from 'react-native'
+import React from 'react'
+import styles from './Welcome.style'
+import { COLORS, SIZES } from '../../constants'
+import { Feather } from '@expo/vector-icons'
+
+const Welcome = () => {
+  return (
+    <View>
+    <View style={styles.container}>
+    <Text style={styles.welcomeTxt(COLORS.black,SIZES.xSmall)}>{" "}Find the most</Text>
+    <Text style={styles.welcomeTxt(COLORS.primary,0)}>{" "}Luxurious furniture</Text>
+    </View>
+    <View style={styles.searchContainer}>
+    <TouchableOpacity>
+    <Feather name='search' size={24} style={styles.searchIcon}/>
+    <View style={styles.searchWrapper}></View>
+    </TouchableOpacity>
+    </View>
+    </View>
+  )
+}
+
+export default Welcome
