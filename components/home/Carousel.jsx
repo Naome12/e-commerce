6fe2529302d1352ad6a0a25defa6911d3ceeb390
Naome <tuyishimenaome27@gmail.com>
2023-11-20 21:40:06 +0200

    adding carousel

--- components/home/Carousel.jsx
+++ components/home/Carousel.jsx
@@ -1,21 +1,94 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SliderBox } from 'react-native-image-slider-box'
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
+import Swiper from 'react-native-swiper/src';
+
+const slides = [
+  require('../../assets/chair2.jpg'),
+  require('../../assets/chair1.jpg'),
+  require('../../assets/chairs.png'),
+];
 
 const Carousel = () => {
-    const slides=[
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/cb2e64a8-ad4c-4d45-b58b-b0c7e11b6bb4-fn1.jpg",
-        "https://d326fntlu7tb1e.cloudfront.net/uploads/b1f6d96d-3297-4270-ba65-657dc2bc0236-fn2.jpg",
-       "https://shorturl.at/wEQW8"
-    ]
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onIndexChanged = (index) => {
+    setCurrentIndex(index);
+  };
+
   return (
-    <View>
-      <SliderBox>{slides}
-      </SliderBox>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Swiper
+          loop={false}
+          showsPagination={true}
+          paginationStyle={styles.paginationContainer}
+          dot={<View style={styles.dot}/>}
+          activeDot={<View style={styles.activeDot}/>}
+          onIndexChanged={onIndexChanged}
+          style={styles.wrapper}
+        >
+          {slides.map((imageUrl, index) => (
+            <View key={index} style={styles.slide}>
+              <Image source={imageUrl} style={styles.image} />
+            </View>
+          ))}
+        </Swiper>
+      </View>
+      </View>
 
-export default Carousel
+       );
+};
 
-const styles = StyleSheet.create({})
+const { width } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: width - 40,
+    height: 200,
+    overflow: 'hidden',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  wrapper: {
+    height: '100%',
+  },
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  paginationContainer:{
+    bottom:10,
+  },
+  dot: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    margin: 5,
+    backgroundColor: '#bbb',
+  },
+  activeDot:{
+    height:10,
+    width:10,
+    borderRadius:5,
+    margin:5,
+    backgroundColor:'#717171'
+  }
+});
+
+export default Carousel;
