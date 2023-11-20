bb3a1842377c5819364a8747fef2af7687edb9f4
Naome <tuyishimenaome27@gmail.com>
2023-11-20 21:40:30 +0200

    adding headings

---
+++ components/home/headings.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+import { COLORS,SIZES } from '../../constants'
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop:SIZES.medium,
+        marginBottom:SIZES.xSmall,
+        marginHorizontal: 12,
+        
+    },
+    header:{
+        flexDirection:"row",
+        justifyContent:'space-between',
+    }
+})
+
+export default styles;
--- components/index.js
+++ components/index.js
@@ -1,5 +1,8 @@
 import Welcome from "./home/Welcome";
+import Carousel from "./home/Carousel";
 
 export {
-    Welcome
+    Welcome,
+    Carousel
+
 }
